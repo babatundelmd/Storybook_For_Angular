42f358ed17e9011a98b84e6c7ef2e8589c46d853
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-01-02 02:13:33 +0100

    🔀merge master branch

--- src/app/Model/Photo.model.ts
+++ src/app/Model/Photo.model.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export interface Photo {
     src?: string;
     title?: string;
-=======
-export interface Photo {
-    src?: string;
-    title?: string;
->>>>>>> 43baa30dd317f7eee29ab0df587fb9433f5d749c
 }
