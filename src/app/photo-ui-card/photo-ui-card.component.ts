87d2468f95f9a2285cbbc629e605b1d530b23924
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-01-02 01:10:45 +0100

    🚧chore: add props

--- src/app/photo-ui-card/photo-ui-card.component.ts
+++ src/app/photo-ui-card/photo-ui-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Photo } from '../Model/Photo.model';
 
 @Component({
   selector: 'app-photo-ui-card',
   templateUrl: './photo-ui-card.component.html',
-  styleUrls: ['./photo-ui-card.component.css']
+  styleUrls: [ './photo-ui-card.component.css' ]
 })
 export class PhotoUiCardComponent implements OnInit {
+  @Input() name: string;
+  @Input() jobDescription: string;
+  @Input() email: string;
+  @Input() phone: string;
+  @Input() photo: Photo;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor () { }
+
+  ngOnInit () {
   }
 
 }
